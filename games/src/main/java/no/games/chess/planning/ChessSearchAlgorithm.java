2ead669d7de848c8514f629851b19f9f85619dde
olufjen <olufjen@gmail.com>
2022-08-23 09:37:13 +0200

    Changes to the chess planning algorithm

--- games/src/main/java/no/games/chess/planning/ChessSearchAlgorithm.java
+++ games/src/main/java/no/games/chess/planning/ChessSearchAlgorithm.java
@@ -15,7 +15,8 @@ import java.util.*;
  * Artificial Intelligence A Modern Approach (3rd Edition): Figure 11.5, page
  * 409.<br>
  * <br>
- * 
+ * @since August 2022
+ * A list  List<ActionSchema> allPrefix has been added to search the initial state of all prefixes
  * <pre>
  * function HIERARCHICAL-SEARCH(problem, hierarchy) returns a solution, or failure
  *  frontier ← a FIFO queue with [Act] as the only element
@@ -44,7 +45,11 @@ public class ChessSearchAlgorithm {
 	private PrintWriter writer =  null;
 	private ChessProblem problem;
 	private List<ActionSchema> reserveplan = new ArrayList<ActionSchema>();
-	private List<ActionSchema> allPrefix = null;
+	private List<List<ActionSchema>> allPrefix = null;
+/*
+ * noPlan is true if the search returns a reserve plan	
+ */
+	private boolean noPlan = false;
 
   public ChessSearchAlgorithm(FileWriter fw, PrintWriter writer) {
 	super();
@@ -68,7 +73,15 @@ public FileWriter getFw() {
 		this.writer = writer;
   }
 
-  /**
+  public boolean isNoPlan() {
+	  return noPlan;
+  }
+
+  public void setNoPlan(boolean noPlan) {
+	  this.noPlan = noPlan;
+  }
+
+/**
    * function HIERARCHICAL-SEARCH(problem, hierarchy) returns a solution, or failure
    * 
    * @param problem The planning problem.
@@ -76,23 +89,30 @@ public FileWriter getFw() {
    */
   public List<ActionSchema> heirarchicalSearch(ChessProblem problem) {
         // frontier ← a FIFO queue with [Act] as the only element
+	  // LinkedList is an implementation of Queue
 	  this.problem = problem;
         Queue<List<ActionSchema>> frontier = new LinkedList<>();
-        allPrefix = new ArrayList<ActionSchema>();
+        allPrefix = new ArrayList<List<ActionSchema>>();
 //        frontier.add(new ArrayList<>(Collections.singletonList(ChessPlanningProblemFactory.getHlaAct(problem))));
         ChessHighLevelAction hlax =  ChessPlanningProblemFactory.getHlaAct(problem);
         String content = hlax.toString();
         writer.println("\nInside chess search The HLA:\n");
         writer.println(content);
         writer.println("** End HLA **\n");
-        frontier.add(Collections.singletonList(hlax));
-    
+        frontier.add(Collections.singletonList(hlax)); // Creates the frontier queue 
+/*
+ * The singletonList() method of java.util.Collections class is used to return an immutable list containing only the specified object.
+ * The returned list is serializable. This list will always contain only one element thus the name singleton list. 
+ * When we try to add/remove an element on the returned singleton list, it would give UnsupportedOperationException.        
+ */
+        int prefixcounter = 0;
         // loop do
         while (true) {
             // if EMPTY?(frontier) then return failure
             if (frontier.isEmpty()) {
             	writer.println("\nEnd chess search \n");
             	writer.flush();
+            	noPlan = true;
             	return reserveplan;
 //                return null;
             }
@@ -100,7 +120,7 @@ public FileWriter getFw() {
             /*
              * At present the plan contains only one HLA action with refinements
              */
-            List<ActionSchema> plan = frontier.poll();
+            List<ActionSchema> plan = frontier.poll(); // Retrieves the head of this queue or null if empty
 /*
  * Added logic: If plan is empty but frontier is not then retrieve next plan olj 15.02.21            
  */
@@ -128,13 +148,16 @@ public FileWriter getFw() {
             for (int j = 0; j < i; j++) {
                 prefix.add(plan.get(j));
             }
-            allPrefix.addAll(prefix);
+            allPrefix.add(prefix);
             for (int j = i + 1; j < plan.size(); j++) {
                 suffix.add(plan.get(j));
             }
+
+            int prefixsize = allPrefix.size();
             // outcome ← RESULT(problem.INITIAL-STATE, prefix)
-//            State outcome = problem.getInitialState().result(prefix);
-            State outcome = problem.getInitialState().result(allPrefix);
+//           State outcome = problem.getInitialState().result(prefix); // Returns with a reserve plan
+            List<ActionSchema> thisprefix = allPrefix.get(prefixcounter); // OBS: Some prefix lists may be empty
+            State outcome = problem.getInitialState().result(thisprefix); // Search for outcome one prefix list at a time olj 11.08.22
             // if hla is null then /* so plan is primitive and outcome is its result See pseudocode p. 409*/
             if (hla == null) {
                 // if outcome satisfies problem.GOAL then return plan
@@ -150,7 +173,7 @@ public FileWriter getFw() {
                         writer.println(l.toString());
                     }
                     return plan;
-                }else { // This is added logic: to make sure to return a spare plan
+                }else { // This is added logic: to make sure to return a spare plan or to test the next set of prefixes
                     List <Literal> goalfluents = outcome.getFluents();
                     List<Literal> statefluents = problem.getGoalState().getFluents();
                     writer.println("The fluents of end outcome:\n");
@@ -160,16 +183,31 @@ public FileWriter getFw() {
                     writer.println("The fluents of goal state:\n");
                     for (Literal l: statefluents) {
                         writer.println(l.toString());
+                    }String pr = "The prefix list is empty";
+                    if (thisprefix != null && !thisprefix.isEmpty() ) {
+                    	ActionSchema s = thisprefix.get(0);
+                    	int prsize = thisprefix.size();
+                    	String n = "none";
+                    	if (s != null)
+                    		n= s.getName();
+                    	pr = "The prefix list contains "+prsize + " elements. And the first is " + n;
                     }
-                	writer.println("\nEnd chess search with a reserve plan \n");
+                    writer.println(pr);
+                    prefixcounter++;
+                	writer.println("\nEnd chess search no plan "+prefixcounter+" No of prefix sets "+prefixsize);
                 	writer.flush();
-                	return reserveplan;
+                	noPlan = true;
+                	if(prefixcounter >= prefixsize) {
+                		writer.println("\nEnd chess search reserveplan"+prefixcounter);
+                     	return reserveplan;
+                	}
+   
                 }
-            } else {
+            } else { // The HLA is not null !!
             	 writer.println("\nHLA is not null \n");
                 List<ActionSchema> tempInsertionList = new ArrayList<>();
                 // else for each sequence in REFINEMENTS(hla, outcome, hierarchy) do
-                for (List<ActionSchema> sequence :
+                for (List<ActionSchema> sequence : // The function refinements returns a list of applicable actionSchemas
                         refinements(hla, outcome)) {
                     // frontier ← INSERT(APPEND(prefix, sequence, suffix), frontier)
                     tempInsertionList.clear();
@@ -190,7 +228,7 @@ public FileWriter getFw() {
    * 
    * @param hla     The hla to which the refinements are to be applied.
    * @param outcome The state in which the refinements are to be applied.
-   * @return List of all refinements of the current hla in a given outcome state.
+   * @return List of all applicable refinements of the current hla in a given outcome state.
    */
   public List<List<ActionSchema>> refinements(ActionSchema hla,State outcome) {
         List<List<ActionSchema>> result = new ArrayList<>();
@@ -207,8 +245,10 @@ public FileWriter getFw() {
         for (List<ActionSchema> refinement :
                 ((ChessHighLevelAction) hla).getRefinements()) {
             if (refinement.size() > 0) {
-                if (outcome.isApplicable(refinement.get(0)))
+                if (outcome.isApplicable(refinement.get(0))) {
                     result.add(refinement);
+                    writer.println("Refinement is applicable "+refinement.get(0).toString());
+                }
             } else
                 result.add(refinement);
         }
