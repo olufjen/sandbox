d8e21ddabf003a4bc801ee015ab04b905c0efbb5
olufjen <olufjen@gmail.com>
2024-07-23 09:40:02 +0200

    Modified function interface

--- games/src/main/java/no/function/FunctionContect.java
+++ games/src/main/java/no/function/FunctionContect.java
@@ -4,21 +4,46 @@ import java.util.HashMap;
 
 /**
  * This class register and defines functions to be executed
+ * These functions are held in a Map with the name of the function as the key.
  * @author Oluf
  *
  */
 public class FunctionContect {
-	 HashMap<String, FunctionExecutor> context=new HashMap<String, FunctionExecutor>();
+	private HashMap<String, FunctionExecutor> context;
 
-	    public void register(String name,FunctionExecutor function){
-	        context.put(name, function);
-	    }
+	public FunctionContect() {
+		super();
+		context = new HashMap<String,FunctionExecutor>();
+	}
+	public HashMap<String, FunctionExecutor> getContext() {
+		return context;
+	}
 
-	   public Object call(String name, String... names){
-	      return    context.get(name).execute(names);
-	   }
+	public void setContext(HashMap<String, FunctionExecutor> context) {
+		this.context = context;
+	}
 
-	   public FunctionExecutor get(String name){
-	      return context.get(name);
-	   }
+	/**
+	 * register
+	 * This method register functions to be used in the context Map
+	 * @param name - The name of the function
+	 * @param function - The function implementing the FunctionExecutor interface
+	 */
+	public void register(String name,FunctionExecutor function){
+		context.put(name, function);
+	}
+
+	public Object call(String name){
+		return    context.get(name).execute();
+	}
+
+	/**
+	 * get
+	 * This method returns the FunctionExecutor with the given name
+	 * @param name - The name of the FunctionExecutor
+	 * @return
+	 */
+	public FunctionExecutor get(String name){
+		return context.get(name);
+	}
 }
--- games/src/main/java/no/function/FunctionExecutor.java
+++ games/src/main/java/no/function/FunctionExecutor.java
@@ -1,6 +1,9 @@
 package no.function;
 
+import java.util.HashMap;
+
 import aima.core.logic.fol.parsing.ast.Constant;
+import aima.core.logic.fol.parsing.ast.Term;
 
 /**
  * This interface is used to define functions to be executed
@@ -8,10 +11,12 @@ import aima.core.logic.fol.parsing.ast.Constant;
  *
  */
 public interface FunctionExecutor {
-	public Object execute(String... names );
-	public Constant findStartpos(String var);
+	public Object execute();
+	public void buildTerms(HashMap<String,Term> cParam,HashMap<String,Term> vParam);
+	
+/*	public Constant findStartpos(String var);
 	public Constant findPiecename(String var);
 	public Constant findNewpos(String var);
 	public Constant findPiecetype(String var);
-	
+	*/
 }
